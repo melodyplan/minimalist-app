625acc67df1f092f4e70486501e919f885fec68e
Melody Plan <melodyplan@gmail.com>
2017-08-14 18:22:07 -0700

    lez goooo

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const cookieSession = require('cookie-session')
+const path = require('path')
 const keys = require('./config/keys')
 
 // initialize app (buy the plot of the land)
 const app = express()
 
 // register middleware (building in the immigration center)
-app.use(cookieSession({
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-  keys: [keys.cookieKey]
-}))
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -21,6 +24,17 @@ require('./models/User')
 // register routes (putting in the road signs)
 app.use('/auth/google', require('./routes/authRouter'))
 app.use('/api', require('./routes/apiRouter'))
+
+// register web routes
+if (process.env.NODE_ENV === 'production') {
+  // serve static files
+  app.use(express.static('/client/dist'))
+
+  // define SPA (single page app) routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
 
 // connect database (creating the map)
 mongoose.connect(keys.mongoURI)
